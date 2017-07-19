3ccff2cbabe46ebd72e26a84f6b261c9bf7d37d7
Ángel <amacdong@usersad.everis.int>
2017-07-19 13:52:39 +0200

    testCommit

--- test.sh
+++ test.sh
@@ -4,6 +4,7 @@ set -o pipefail
 
 ERRORS=()
 
+# test
 # find all executables and run `shellcheck`
 for f in $(find . -type f -not -iwholename '*.git*' -not -name "yubitouch.sh" | sort -u); do
 	if file "$f" | grep --quiet shell; then
